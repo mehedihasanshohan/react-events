2559772ea6ea3ba267b289bd2ed7e224c040d558
mehedi hasan <mhshohan04@gmail.com>
2025-04-10 11:31:13 +0600

    adding a state variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import './App.css'
+import { useState } from 'react';
+import { sculptureList } from './data.js';
 
-function App() {
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
 
+  function handleClick() {
+    setIndex(index + 1);
+  }
+
+  let sculpture = sculptureList[index];
   return (
-   <>
-    <h2 className='text-rose-700 font-bold text-2xl text-center'>Responding to events</h2>
-   </>
-  )
+    <div className='pl-8 pt-4'>
+      <button className='p-2 bg-blue-400 border-2 rounded-xl text-white' onClick={handleClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img
+        src={sculpture.url}
+        alt={sculpture.alt}
+      />
+      <p>
+        {sculpture.description}
+      </p>
+    </div>
+  );
 }
-
-export default App
