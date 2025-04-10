eebb4589c77730a47afdfac132cb7478b6bfff31
mehedi hasan <mhshohan04@gmail.com>
2025-04-10 11:10:19 +0600

    projects setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+    <h2 className='text-rose-700 font-bold text-2xl text-center'>Responding to events</h2>
+   </>
+  )
+}
+
+export default App
